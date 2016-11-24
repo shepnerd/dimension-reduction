9ab0eddad63546e5704e2681a820d9246ef6fd4e
Wang.Liting <wygamle@pku.edu.cn>
2016-11-24 16:55:46 +0800

    multiple dimensional scaling in function form

---
+++ mds.m
@@ -0,0 +1,35 @@
+function [ ret ] = mds( data, dimension )
+% a simple implementation of multiple dimensional scaling
+% data : a d*n data matrix where each record is col vector of size d*1
+% dimension : desired dimension and dimension b is less than d
+% ret : a dimension-reduced data matrix (b*n)
+
+n = size(data,2);
+
+% distance matrix
+D = zeros(n, n);
+for i = 1 : n
+    residual = bsxfun(@minus, data(:,i:n), data(:,i));
+    D(i,i:n) = sum(residual.^2);
+end
+D = D + D';
+di_ = sum(D,2) / n;
+d_j = sum(D,1) / n;
+d__ = sum(sum(D))/ n / n;
+
+Di_ = repmat(di_,[1 n]);
+D_j = repmat(d_j,[n 1]);
+D__ = repmat(d__,[n n]);
+
+% B = data_compressed'*data_compressed
+B = -0.5*(D-Di_-D_j+D__);
+
+[~,S,V] = svd(B);
+s = diag(S);
+[~,idx] = sort(s,'descend');
+idx = idx(1:dimension);
+
+ret = diag(s(idx)).^0.5*V(:,idx)';
+
+end
+
--- mds_demo.m
+++ mds_demo.m
@@ -6,32 +6,34 @@ load mnist;
 data = teData;
 label = teLabel;
 
+d = 3;
+
 n = size(data,2);
 
-% distance matrix
-D = zeros(n, n);
-for i = 1 : n
-    residual = bsxfun(@minus, data(:,i:n), data(:,i));
-    D(i,i:n) = sum(residual.^2);
-end
-D = D + D';
-di_ = sum(D,2) / n;
-d_j = sum(D,1) / n;
-d__ = sum(sum(D))/ n / n;
-
-Di_ = repmat(di_,[1 n]);
-D_j = repmat(d_j,[n 1]);
-D__ = repmat(d__,[n n]);
-
-% B = data_compressed'*data_compressed
-B = -0.5*(D-Di_-D_j+D__);
-
-[U,S,V] = svd(B);
-s = diag(S);
-[~,idx] = sort(s,'descend');
-
-d = 3;
-data_compressed = diag(s(idx)).^0.5*V(:,idx)';
+data_compressed = mds(data, d);
+%% distance matrix
+% D = zeros(n, n);
+% for i = 1 : n
+%     residual = bsxfun(@minus, data(:,i:n), data(:,i));
+%     D(i,i:n) = sum(residual.^2);
+% end
+% D = D + D';
+% di_ = sum(D,2) / n;
+% d_j = sum(D,1) / n;
+% d__ = sum(sum(D))/ n / n;
+% 
+% Di_ = repmat(di_,[1 n]);
+% D_j = repmat(d_j,[n 1]);
+% D__ = repmat(d__,[n n]);
+% 
+%% B = data_compressed'*data_compressed
+% B = -0.5*(D-Di_-D_j+D__);
+% 
+% [U,S,V] = svd(B);
+% s = diag(S);
+% [~,idx] = sort(s,'descend');
+% idx = idx(1:d);
+% data_compressed = diag(s(idx)).^0.5*V(:,idx)';
 
 label_class = unique(label);
 
@@ -44,7 +46,7 @@ for i = 1 : 5
     hold on;
 end
 grid on;
-saveas(gcf, 'visualization_digits_0-4.jpg');
+saveas(gcf, 'visualization_digits_0-4_mds.jpg');
 
 
 
